56575c99f4454b587934b53410b21bef2c506c6b
Jason Viola <jason.viola@mavs.uta.edu>
2019-08-24 20:01:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,13 @@ class Song
   
   def self.genre_count
     genre_count = {}
-    @@genres.each 
+    @@genres.each do {|genre| 
+    if genre_count[genre]
+      genre_count[genre] += 1
+    else
+      genre_count = genre => 1
+    end
+    }
     
   
   def self.artists
